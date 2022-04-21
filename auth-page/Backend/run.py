874bf9e16292ded781fec436988c110019ca235e
Abenezer Molla <57775399+abenezer-molla@users.noreply.github.com>
2022-04-21 13:10:42 +0100

    Clearing out unnecessary lines

--- auth-page/Backend/run.py
+++ auth-page/Backend/run.py
@@ -1,8 +1,1 @@
-from main import create_app
-from config import DevConfig
-
-
-if __name__ == '__main__':
-    app = create_app(DevConfig)
-    app.run()
 
--- auth-page/src/Components/HomeDisc.js
+++ auth-page/src/Components/HomeDisc.js
@@ -3,12 +3,12 @@ import './App.css';
 import { Button } from './Button';
 import './HomeDisc.css';
 
-function HeroSection() {
+function HomeDisc() {
   return (
     <div className='hero-container'>
       
       <h1>WELCOME</h1>
-      <p>WE ARE HERE TO SERVE YOU</p>
+        <p>WE ARE HERE TO SERVE YOU</p>
       <div className='hero-btns'>
         <Button
           className='btns'
@@ -17,17 +17,10 @@ function HeroSection() {
         >
           GET STARTED
         </Button>
-        <Button
-          className='btns'
-          buttonStyle='btn--primary'
-          buttonSize='btn--large'
-          onClick={console.log('hey')}
-        >
-          WATCH INTRO VIDEO<i className='far fa-play-circle' />
-        </Button>
+
       </div>
     </div>
   );
 }
 
-export default HeroSection;
+export default HomeDisc;
